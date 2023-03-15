081d98d17969af16c1aa20ceea0e9af1fa35033c
YC <conyang@publicisgroupe.net>
2023-03-15 14:58:13 +0800

    11

---
+++ uni-app/demo-imooc/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="pageIndex">
+		<view class="pageIndex__input">
+			<uni-easyinput prefixIcon="search"  v-model="formLine.searchValue" placeholder="请输入" @iconClick="iconClick" />
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formLine:{
+					searchValue:''
+				},
+			
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			iconClick(){
+				
+			}
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.pageIndex{
+		&__input{
+			padding: 0 20px 0 20px;
+			overflow: hidden;
+	
+		
+		
+			
+		}
+		
+	}
+	
+</style>
