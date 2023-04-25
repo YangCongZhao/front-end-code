f3837dadde9882b7fed5e1ac619358246eee3184
洋_葱 <88417823@qq.com>
2023-04-25 10:59:43 +0800

    11
    11

--- uni-app/demo-imooc/pages/my/my.vue
+++ uni-app/demo-imooc/pages/my/my.vue
@@ -1,6 +1,35 @@
 <template>
-	<view>
-		
+	<view class="page page-fill">
+		<view class="header">
+			<view class="info-wapper">
+				<view v-if="userIsLogin">
+						<image src="../../static/icos/108x108.png" class="face" mode=""></image>
+				</view>
+				<view v-else>
+					<image src="../../static/icos/QQ.png" class="face" mode=""></image>
+				</view>
+			
+			<view v-if="userIsLogin">
+				<view class="nickname">
+					yc
+				</view>
+				<view class="nav-info">
+					id:3333232323
+				</view>
+			</view>
+			<view v-else>
+				<text @click="sinUp">
+					注册:
+				</text>
+				<text>登录</text>
+				
+			</view>
+			</view>
+			<view class="set-wapper">
+				<image src="../../static/icos/settings.png" class="settings" mode=""></image>	
+			</view>
+			
+		</view>
 	</view>
 </template>
 
@@ -8,12 +37,22 @@
 	export default {
 		data() {
 			return {
-				
+				userIsLogin:false
 			};
+		},
+		methods:{
+			login(){
+				this.userIsLogin = true
+			},
+			sinUp(){
+				uni.navigateTo({
+					url:'/pages/sinUp/sinUp'
+				})
+			}
 		}
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+@import url('my.scss');
 </style>
