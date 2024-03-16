9c8fa642c0dc9d8af6c7d66366f5d4798cc3bcf6
YC-LAPTOP\88417 <15315575962@163.com>
2024-03-16 16:20:11 +0800

    1

--- TS/baseTs/src/index.ts
+++ TS/baseTs/src/index.ts
@@ -1,2 +1,8 @@
 let a:string = '23'
-console.log(a);
+let tul:[string,number,mael:boolean] = ['yc',26,true]
+const enum USER_ROLE{
+    SUPER_ADMIN,
+    USER
+}
+let c = USER_ROLE.SUPER_ADMIN
+console.log(c);
