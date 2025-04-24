131392cf5659504d86adf77868c2c113fde4ce48
conyang <yangcongzhao123@gmail.com>
2025-04-24 16:13:03 +0800

    欢乐购

--- Node/baseCore/readSystem/index.js
+++ Node/baseCore/readSystem/index.js
@@ -9,14 +9,9 @@ function getPath(val) {
     return path.resolve(__dirname, val)
 }
 
-fs.readFile(getPath('./a.txt'), 'utf8', function (err, data) {
-    console.log(err, data);
-})
-//写入文件 会整个清空
-fs.writeFile(getPath('./a.txt'), 'utf8heihei', function (err, data) {
-
-})
-//写入文件 追加
-fs.appendFile(getPath('./a.txt'), '追加哈哈', function (err, data) {
-
+fs.stat(getPath('users'), (err, stats) => {
+    if (err) {
+        console.error(err)
+    }
+    console.log(stats);
 })
