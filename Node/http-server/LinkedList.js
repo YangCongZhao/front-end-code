d7f037906b5e09f321ab256124a700e3f2e51c1e
conyang <yangcongzhao123@gmail.com>
2025-05-13 17:59:56 +0800

    init

--- Node/http-server/LinkedList.js
+++ Node/http-server/LinkedList.js
@@ -47,15 +47,56 @@ function LList(item){
     this.findPrevious  = findPrevious
 }
 
-let l1 = new LList(2)
-l1.insert(4,2)
-l1.insert(3,4)
-let l2 = new LList(5)
-l2.insert(6,5)
-l2.insert(4,6)
+let l1 = new LList(1)
+l1.insert(1,1)
+l1.insert(1,1)
+l1.insert(2,1)
 
 
-var addTwoNumbers = function(l1, l2) {
-      
+var deleteDuplicates = function(head) {
+    let currNode = head;
+    while (currNode && currNode.next) {
+        if (currNode.val === currNode.next.val) {
+            currNode.next = currNode.next.next; // Skip the duplicate node
+        } else {
+            currNode = currNode.next; // Move to the next node
+        }
+    }
+    return head;
 };
-console.log(addTwoNumbers(l1,l2))
+
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next; // Move slow pointer by 1 step
+        fast = fast.next.next; // Move fast pointer by 2 steps
+
+        if (slow === fast) {
+            return true; // Cycle detected
+        }
+    }
+    return false; // No cycle
+    
+};
+
+
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    let intersectionSet = new Set([...set1].filter(x => set2.has(x)));
+    return Array.from(intersectionSet);
+    //换一种写法 不要用api 
+    let result = []
+    for(let i = 0; i < nums1.length; i++){
+        for(let j = 0; j < nums2.length; j++){
+            if(nums1[i] == nums2[j]){
+                result.push(nums1[i])
+            }                        
+        }
+    }
+
+    return result
+};
+console.log(intersection([4,9,5],[9,4,9,8,4]))
